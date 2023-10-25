5db2fe005906b558fde07bed1b7889c5f0558e9e
Rahul <rahul.khatri@eon.com>
2023-10-26 00:49:19 +0500

    Initial commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <div>
+        <div class="p-6 bg-white border-b border-gray-200 lg:p-8">
+            <ApplicationLogo class="block w-auto h-12" />
+
+            <h4 class="mt-8 text-2xl font-medium text-gray-900">
+                Welcome to Mini Admin Panel
+            </h4>
+
+            
+
+           
+
+        </div>
+    </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './Layouts/AppLayout.vue'
+
+createApp(App).mount('#app')
